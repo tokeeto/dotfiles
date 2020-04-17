1167b2d3e1e81efe46eb285daa0bd59ed5608616
Toke Ivø <toke.ivo@gmail.com>
2020-04-17 19:19:17 +0200

    second commit

---
+++ init.el
@@ -0,0 +1,17 @@
+
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/toke_init.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (monokai-theme zenburn-theme yasnippet web-mode web-beautify swiper smooth-scrolling smex smart-tabs-mode simpleclip rainbow-mode projectile powerline multiple-cursors magit keyfreq iy-go-to-char ivy-xref ivy ido-completing-read+ ido-vertical-mode htmlize flx-ido expand-region engine-mode diminish csharp-mode better-defaults avy auto-complete ag use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
