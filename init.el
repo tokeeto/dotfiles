815a126cff3d295bdd188008def3f3719d9b0dec
Toke Ivø <toke.ivo@gmail.com>
2020-07-06 16:27:54 +0200

    improvements

--- init.el
+++ init.el
@@ -6,9 +6,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(macoy-selected-desktop "~/.emacs.d/macoy-desktops/astralica")
  '(package-selected-packages
    (quote
-    (monokai-theme zenburn-theme yasnippet web-mode web-beautify swiper smooth-scrolling smex smart-tabs-mode simpleclip rainbow-mode projectile powerline multiple-cursors magit keyfreq iy-go-to-char ivy-xref ivy ido-completing-read+ ido-vertical-mode htmlize flx-ido expand-region engine-mode diminish csharp-mode better-defaults avy auto-complete ag use-package))))
+    (ample-theme monokai-theme zenburn-theme yasnippet web-mode web-beautify swiper smooth-scrolling smex smart-tabs-mode simpleclip rainbow-mode projectile powerline multiple-cursors magit keyfreq iy-go-to-char ivy-xref ivy ido-completing-read+ ido-vertical-mode htmlize flx-ido expand-region engine-mode diminish csharp-mode better-defaults avy auto-complete ag use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
