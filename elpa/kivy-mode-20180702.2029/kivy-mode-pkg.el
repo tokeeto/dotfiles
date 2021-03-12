3daaf041aa8c20b711182602b8cbb23b8873b663
Toke Ivø <toke.ivo@gmail.com>
2021-03-12 19:53:42 +0100

    next update

---
+++ elpa/kivy-mode-20180702.2029/kivy-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /home/toke/.emacs.d/elpa/kivy-mode-20180702.2029/kivy-mode.el  -*- no-byte-compile: t -*-
+(define-package "kivy-mode" "20180702.2029" "Emacs major mode for editing Kivy files" 'nil :commit "1a7cd9cfdeaac07df0913f1901856991a0d46436" :authors '(("Dean Serenevy" . "dean@serenevy.net")) :maintainer '("Dean Serenevy" . "dean@serenevy.net"))
